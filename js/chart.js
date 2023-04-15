faca3d76f6ee99aacb5e67bcee8903a4d76968ed
ahahron0304 <77768052+ahahron0304@users.noreply.github.com>
2023-04-15 11:03:28 +0800

    Add files via upload

---
+++ js/chart.js
@@ -0,0 +1,49 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var donations = document.getElementById('donations').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Shelter', 'Education', 'Medical'],
+        datasets: [{
+            label: 'Donations for:',
+            data: [700, 200, 400],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)'
+              ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
+
+var myChart = new Chart(donations, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+         'October', 'November', 'December'],
+        datasets: [{
+            label: 'Donations',
+            data: [1200, 1090, 3000, 5400, 1220, 3600, 4800, 4578, 2566, 8545, 4587, 9000],
+            backgroundColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+        }]
+    },
+    options: {
+        responsive: true,
+    }
+});
